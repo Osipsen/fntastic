592141d473917b4d75071a2521a62a0b0737f4a6
osipsen <osipsen@mail.ru>
2021-12-06 17:03:39 +0900

    FCommit

---
+++ FNTASTIC/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите строку");
+String stroka = Console.ReadLine().ToLower();
+String stroka2 = "";
+for (int i = 0, F = 0, N = stroka.Length; i < N; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        if (stroka[i] == stroka[j]) F=F+1;
+        if (F > 1)
+        {
+            stroka2 = stroka2+ ")";
+            break;
+        };
+    }
+    if (F == 1) stroka2 = stroka2 + "(";
+    F= 0;
+}
+Console.WriteLine(stroka2);
